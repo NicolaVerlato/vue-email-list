54f08b28447b233f05520c05ad0f62d55368519b
NicolaVerlato <nicola.verlato00@gmail.com>
2022-07-04 15:32:07 +0200

    first commit

---
+++ bonus/js/script.js
@@ -0,0 +1,19 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            emails: []
+        }, 
+        methods: {
+        },
+        mounted() {
+            for(let i = 1; i <= 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    const randomEmail= response.data.response;
+                    this.emails.push(randomEmail)
+                });
+            }
+        }
+    }
+)
